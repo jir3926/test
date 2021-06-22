8a2e59f1932731fd34451427b26e7df3094942f8
Nick <jir3926@gmail.com>
2021-06-22 23:02:18 +0800

    20210622 test

---
+++ 排班系統/排班系統/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace 排班系統
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21"});
+            this.listBox1.Location = new System.Drawing.Point(26, 159);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 184);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "新增";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "刪除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(185, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "插入";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(266, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "清除";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "請輸入員工姓名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(409, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(386, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "自動排班";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(464, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(402, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "上班人數:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(234, 159);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 184);
+            this.listBox2.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "總員工人數";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "白斑";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Location = new System.Drawing.Point(360, 159);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 184);
+            this.listBox3.TabIndex = 15;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 12;
+            this.listBox4.Location = new System.Drawing.Point(486, 159);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 184);
+            this.listBox4.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(376, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "小夜班";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(513, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "大夜班";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(152, 159);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "自動排班別";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(152, 188);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "自動排班別";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "白斑",
+            "小夜班",
+            "大夜班"});
+            this.comboBox1.Location = new System.Drawing.Point(467, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ 排班系統/排班系統/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 排班系統
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!= "")
+            {
+                listBox1.Items.Add(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("請輸入員工姓名", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("請選擇刪除對象", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.Insert(listBox1.SelectedIndex, textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("請選擇插入位置", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";                       
+            Random r = new Random();
+            int attendance = 0;
+            string[] name = new string[listBox1.Items.Count];
+            int[] namm = new int[10];
+            //namm[0] = r.Next(1, attendance + 1);
+
+            for (int i = 0; i < listBox1.Items.Count; i++)  //將全部員工姓名加至字串陣列
+            {
+                name[i] = Convert.ToString(listBox1.Items[i]);
+            }
+            if (int.TryParse(textBox2.Text, out attendance)&& attendance<=name.Length)
+            { //判斷是否為數測並隨機分配上班人員
+                for (int i = 0; i < attendance; i++)
+                {
+                    namm[i] = r.Next(1, listBox1.Items.Count);
+                    //MessageBox.Show(name[i] + "=i");
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (namm[i] == namm[j])
+                        {
+                            namm[i] = r.Next(1, listBox1.Items.Count);
+                            j--;
+                        }
+
+                    }
+
+                    label2.Text += name[namm[i]] + "\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入正確人數", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int x = 0;
+           
+            List<int> xyz = new List<int>();
+
+            List<ListBox> listbb = new List<ListBox>();
+            listBox2.Items.Clear(); listBox3.Items.Clear(); listBox4.Items.Clear();
+
+            listbb.Add(listBox2); listbb.Add(listBox3); listbb.Add(listBox4);
+
+            for (int i = 0; i < 18 ; i++)         //插入隨機班別
+            {
+                if (listBox2.Items.Count < 8) //白斑人數未滿足
+                {
+                    if (listBox3.Items.Count < 6) //白斑未滿小夜斑未滿
+                    {
+                        if (listBox4.Items.Count < 4)//大夜未滿
+                        {
+                            x = r.Next(0, 3);  //白斑小夜大夜隨機分配
+                        }
+                        else
+                        {
+                            x = r.Next(0, 2);
+                            // MessageBox.Show("排班結束");
+                        }
+                    }
+                    else//小夜已滿
+                    {
+                        if (listBox4.Items.Count < 4)// 白斑未滿小夜已滿大夜未滿
+                        {
+                            x = r.Next(0, 2);
+                            if (x == 1)
+                            {
+                                x = 2;
+                            }
+                        }
+                        else
+                        {
+                            x = r.Next(0, 1);
+                        }
+                    }
+                }
+                else //白斑人數已滿足
+                {
+                    if (listBox3.Items.Count < 6)
+                    {
+                        if (listBox4.Items.Count < 4)
+                        {
+                            x = r.Next(1, 3);
+                        }
+                        else
+                        {
+                            x = r.Next(1,2);
+                        }
+                    }
+                    else
+                    {
+                        if (listBox4.Items.Count < 4)
+                        {
+                            x = r.Next(2, 3);
+                        }
+                        else
+                        {
+                            MessageBox.Show("排班結束");
+                        }
+                    }
+                }
+
+                xyz.Add(r.Next(1, listBox1.Items.Count));  //插入隨機員工
+                for (int z = 0; z < i; z++)
+                {
+                    if (xyz[i] == xyz[z])
+                    {
+                        xyz[i] = r.Next(1, listBox1.Items.Count );
+                        z--;
+                    }
+                }
+                listbb[x].Items.Add(Convert.ToString(listBox1.Items[xyz[i]]));
+
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Random r = new Random();
+           
+
+            //List<int> xyz = new List<int>();
+            //label2.Text = "";
+            //for (int y = 0; y < 18; y++)
+            //{
+            //    xyz.Add(r.Next(1, listBox1.Items.Count+1));
+            //    for (int z = 0; z < y; z++)
+            //    {
+            //        if (xyz[y] == xyz[z])
+            //        {
+            //            xyz[y] = r.Next(1, listBox1.Items.Count+1);
+            //            z--;
+            //        }
+            //    }
+            //    //listbb[x].Items.Add(Convert.ToString(listBox1.Items[xyz[y]]));
+            //    label2.Text += xyz[y]+"   ";
+            //}
+        }
+    }
+}
